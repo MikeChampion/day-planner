62af6ccb14c1c4c9dcda20a5b9fa471e233199b6
Zathras <ChampionMike42@gmail.com>
2021-06-05 08:34:23 -0500

    Added getItem for all 'hour' divs

--- main.js
+++ main.js
@@ -28,3 +28,10 @@ let timeCheck = setInterval(updateTime, 30000);
 
 $("#9a .description").val(localStorage.getItem("9a"));
 $("#10a .description").val(localStorage.getItem("10a"));
+$("#11a .description").val(localStorage.getItem("11a"));
+$("#12p .description").val(localStorage.getItem("12p"));
+$("#1p .description").val(localStorage.getItem("1p"));
+$("#2p .description").val(localStorage.getItem("2p"));
+$("#3p .description").val(localStorage.getItem("3p"));
+$("#4p .description").val(localStorage.getItem("4p"));
+$("#5p .description").val(localStorage.getItem("5p"));
