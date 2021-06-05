43eee45aa863434862fd8098ac06780cf229cabf
Zathras <ChampionMike42@gmail.com>
2021-06-05 08:43:49 -0500

    Set interval for check of current hour

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
-// $(document).ready(function() {
-// 	$('.saveBtn').on('click', function () {
-// 		let value = $(this).siblings('.description').val();
-// 		let hour = $(this).parent().attr('id');
-
-// 		localStorage.setItem(hour, value);
-// 	})
-// })
-
 document.querySelectorAll(".saveBtn").forEach((btn) => {
     btn.addEventListener("click", saveTask);
 });
+let hours = document.querySelectorAll(".time-block");
 
 function saveTask() {
     let hour = $(this).parent().attr("id");
@@ -20,6 +12,7 @@ function saveTask() {
 
 function updateTime() {
     var currentHour = moment().hours();
+    // console.log(hours);
 }
 
 updateTime();
